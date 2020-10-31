a20dc77641ed247f8e6b2381c9ff4112c48413bf
Tanupreet Kaur <200440642@student.georgianc.on.ca>
2020-10-30 23:23:35 -0400

    Stage image and stage title added!

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +13,11 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/Views/barGraphView.fxml"));
+
         Scene scene = new Scene(root);
+        stage.getIcons().add(new Image("/Views/crimes.png"));
+        stage.setTitle("CrimeRecords");
+        scene.getStylesheets().add("stylesheet.css");
         stage.setScene(scene);
         stage.show();
     }
